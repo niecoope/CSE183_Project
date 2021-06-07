876e0e7632a94c1a7306a180ea68f2388dcc5ebb
Eric Tang <etang12@ucsc.edu>
2021-06-06 18:07:39 -0700

    added private folder

---
+++ private/secret_settings.py
@@ -0,0 +1,4 @@
+# single sign on Google (will be used if provided)
+OAUTH2GOOGLE_CLIENT_ID = "1007389198952-76fh92afs3po3fb4u578hotk6omgnta2.apps.googleusercontent.com"
+OAUTH2GOOGLE_CLIENT_SECRET = "AEx2H5OGokE-Sw2RiHiIUNpP"
+
